43921cc6638962e34ba9a62f3b80ecfffe2845bf
yal103 <yal103@ucsd.edu>
2025-05-02 18:20:23 -0700

    expose.js: get elements, horn selection change

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,28 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    // get elements
+    const hornImg = document.querySelector('main > section > img');
+    const selection = document.getElementById('horn-select');
+    const slider = document.querySelector("[type='range']");
+    const volumeImg = document.querySelector('#volume-controls img');
+    const playBtn = document.querySelector('button');
+    const audio = document.querySelector('audio');
+    const confetti = new JSConfetti();
+    // console.log(hornImg);
+    // console.log(selection);
+    // console.log(slider);
+    // console.log(volumeImg);
+    // console.log(playBtn);
+    // console.log(audio);
+    // console.log(confetti);
+
+    // on horn selection change
+    selection.addEventListener('change', function() {
+        const selectedHorn = selection.value;
+        hornImg.src = `assets/images/${selectedHorn}.svg`;
+        audio.src = `assets/audio/${selectedHorn}.mp3`
+        // console.log(hornImg.src);
+        // console.log(audio.src);
+    });
 }
