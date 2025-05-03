7aea5f3d1ac3a45b3298181bb901b1c5554dce60
yal103 <yal103@ucsd.edu>
2025-05-02 20:59:41 -0700

    add isPhoneNumber tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,20 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// isPhoneNumber tests
+test('valid number: xxx-xxx-xxxx', () => {
+    expect(isPhoneNumber('000-000-0000')).toBe(true);
+});
+
+test('valid number: (xxx) xxx-xxxx', () => {
+    expect(isPhoneNumber('(000) 000-0000')).toBe(true);
+});
+
+test('invalid number (no \'-\')', () => {
+    expect(isPhoneNumber('0000000000')).toBe(false);
+})
+
+test('invalid number (letters)', () => {
+    expect(isPhoneNumber('aaa-aaa-aaaa')).toBe(false);
+})
