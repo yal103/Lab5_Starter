e1b22a78c4ef0d8dd83e292fb341ba472cf9af2e
yal103 <yal103@ucsd.edu>
2025-05-02 19:59:47 -0700

    explore.js: get elements, populate voices

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,35 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    const synth = window.speechSynthesis;
+
+    // get elements
+    const face = document.querySelector('img');
+    const input = document.getElementById('text-to-speak');
+    const voiceSelect = document.getElementById('voice-select');
+    const talkBtn = document.querySelector('button');
+
+    // populate voices
+    let voices = [];
+    function populateVoiceList() {
+        voices = synth.getVoices();
+        
+        for (let i = 0; i < voices.length; i++) {
+            const option = document.createElement('option');
+            option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+            if (voices[i].default) {
+                option.textContent += ' - DEFAULT';
+            }
+
+            option.setAttribute('data-lang', voices[i].lang);
+            option.setAttribute('data-name', voices[i].name);
+            voiceSelect.appendChild(option);
+        }
+    }
+    
+    populateVoiceList();
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+        speechSynthesis.onvoiceschanged = populateVoiceList;
+    }
 }
