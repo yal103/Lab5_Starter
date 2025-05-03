5a0c26f781d84e5e6fdb9117777b2448db57cf97
yal103 <yal103@ucsd.edu>
2025-05-02 21:04:26 -0700

    add isEmail tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -19,10 +19,28 @@ test('valid number: (xxx) xxx-xxxx', () => {
     expect(isPhoneNumber('(000) 000-0000')).toBe(true);
 });
 
-test('invalid number (no \'-\')', () => {
+test('invalid number: no \'-\'', () => {
     expect(isPhoneNumber('0000000000')).toBe(false);
-})
+});
 
-test('invalid number (letters)', () => {
+test('invalid number: letters', () => {
     expect(isPhoneNumber('aaa-aaa-aaaa')).toBe(false);
-})
+});
+
+
+// isEmail tests
+test('valid email', () => {
+    expect(isEmail('gmail@gmail.com')).toBe(true);
+});
+
+test('valid email', () => {
+    expect(isEmail('u_c_s_d@ucsd.edu')).toBe(true);
+});
+
+test('invalid email: no @', () => {
+    expect(isEmail('yal103ucsd.edu')).toBe(false);
+});
+
+test('invalid email: no domain', () => {
+    expect(isEmail('gmail@.com')).toBe(false);
+});
