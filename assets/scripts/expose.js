7cc32f34ab41cd1ee20f7311dded971b4231b7c8
yal103 <yal103@ucsd.edu>
2025-05-02 18:53:19 -0700

    expose.js: implement on slider change

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -6,7 +6,7 @@ function init() {
     // get elements
     const hornImg = document.querySelector('main > section > img');
     const selection = document.getElementById('horn-select');
-    const slider = document.querySelector("[type='range']");
+    const slider = document.getElementById('volume');
     const volumeImg = document.querySelector('#volume-controls img');
     const playBtn = document.querySelector('button');
     const audio = document.querySelector('audio');
@@ -26,5 +26,22 @@ function init() {
         audio.src = `assets/audio/${selectedHorn}.mp3`
         // console.log(hornImg.src);
         // console.log(audio.src);
+    });
+
+    // on slider change
+    slider.addEventListener('input', function() {
+        const volume = slider.value;
+        audio.volume = volume / 100;
+        // console.log(audio.volume);
+
+        if (volume == 0) {
+            volumeImg.src = 'assets/icons/volume-level-0.svg';
+        } else if (volume < 33) {
+            volumeImg.src = 'assets/icons/volume-level-1.svg';
+        } else if (volume < 67) {
+            volumeImg.src = 'assets/icons/volume-level-2.svg';
+        } else {
+            volumeImg.src = 'assets/icons/volume-level-3.svg';
+        }
     });
 }
